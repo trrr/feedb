558da20c238d48779f7393fc8d55a1e3584db35e
trrr <onegalgo@gmail.com>
2014-01-21 21:34:49 +0400

    Refactor user model spec

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,12 +1,17 @@
 require 'spec_helper'
 
 describe User do
-  let(:user) { FactoryGirl.create(:user) }
-  it "sets email" do
-    expect(user.email).to eq 'email@example.com'
-  end
-
   it {should have_many(:feedbacks)}
-  it {should have_many(:comments).through(:feedbacks)}
 
+  describe "User" do
+    let(:user) { FactoryGirl.create(:user) }
+
+    it "is valid" do
+      expect(user).to be_valid
+    end
+
+    it "sets email" do
+      expect(user.email).to eq 'email@example.com'
+    end
+  end
 end
