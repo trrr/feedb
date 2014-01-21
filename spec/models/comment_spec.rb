fcb53a071d3e5b662bf0e8d6d70070592997d822
trrr <onegalgo@gmail.com>
2014-01-21 21:35:22 +0400

    Test validness of new comment

--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -5,4 +5,13 @@ describe Comment do
   it {should belong_to(:feedback)}
   it {should validate_presence_of(:content)}
   it {should validate_presence_of(:feedback_id)}
+
+  describe "Comment" do
+    let(:comment) {Comment.new(content: "Text text text", feedback_id: 1)}
+
+    it "is valid" do
+      expect(comment).to be_valid
+    end
+
+  end
 end
